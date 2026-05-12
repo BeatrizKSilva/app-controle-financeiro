8092894228eced9d217f28a0ade60fd65336b9a7
CarlosTito <158493851+CarlosEduardoTito@users.noreply.github.com>
2026-05-12 15:45:34 -0300

    Concluída a lógica de login, registo, logout e recuperação de senha no Controller

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,6 +1,12 @@
-import 'package:vinta_financas/repositories/user_mock.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:vinta_financas/models/usuario_model.dart';
 
 class LoginController {
+  
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   String? validarEmail(String? value) {
     String emailTratado = value!.trim();
 
@@ -25,11 +31,142 @@ class LoginController {
     return null;
   }
 
-  bool fazerLoginMock(String email, String senha) {
-    if (email == UserMock.email && senha == UserMock.senha) {
-      return true;
-    } else {
-      return false;
+  Future<String?>registrarUsuario({
+    
+    required String nome,
+    required String email,
+    required String senha,    
+
+  }) async {
+    try {
+
+      UserCredential credencial = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: senha,
+      );
+
+      User? usuarioFirebase = credencial.user;
+
+      if (usuarioFirebase != null) {
+
+        Usuario novoUsuario = Usuario(
+          id: usuarioFirebase.uid,
+          nome: nome,
+          email: email,
+        );
+
+        await _firestore
+          .collection('usuarios')
+          .doc(usuarioFirebase.uid)
+          .set(novoUsuario.toMap());
+
+        return null;
+
+      }
+    } on FirebaseAuthException catch (e) {
+
+      if (e.code == 'weak-password') {
+
+        return 'A senha é muito fraca.';
+
+      } else if (e.code == 'email-already-in-use') {
+
+        return 'O e-mail já está em uso.';
+
+      }
+    } catch (e) {
+
+      return 'Erro desconhecido ao criar a conta.';
+
     }
   }
+
+  Future<String?> fazerLogin({
+    required String email,
+    required String senha,
+  }) async {
+
+    try {
+
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: senha,
+        );
+
+        return null;
+    
+    } on FirebaseAuthException catch (e) {
+
+      if (e.code == 'user-not-found') {
+
+        return 'Usuario não encontrado.';
+
+      } else if (e.code == 'wrong-password') {
+
+        return 'senha incorreta.';
+
+      } else if (e.code == 'invalid-email') {
+
+        return 'E-mail inválido.';
+
+      } else if (e.code == 'user-disabled') {
+
+        return 'Este usuário foi desabilitado.';
+
+      }
+      return 'Erro ao entrar: ${e.message}';
+
+    } catch (e) {
+
+      return 'Erro desconhecido ao entrar.';
+
+    }
+  }
+
+  Future<void> fazerLogout() async {
+
+    try {
+      await _auth.signOut();
+    
+    } catch (e) {
+
+      print('Erro ao fazer logout: $e');
+
+    }
+
+  }
+
+  Future<String?> recuperarSenha(String email) async {
+
+    try {
+
+      if (email.trim().isEmpty) {
+        return 'Por favor, digite o seu e-mail para recuperar a senha.';
+      }
+
+      await _auth.sendPasswordResetEmail(email: email.trim());
+
+      return null;
+
+    } on FirebaseAuthException catch (e) {
+
+      if (e.code == 'user-not-found') {
+
+        return 'Não encontrámos nenhuma conta associada a este e-mail.';
+        
+      } if (e.code == 'invalid-email') {
+
+        return 'O e-mail fornecido não é válido.';
+
+      }
+      return 'Erro ao pedir recuperação: ${e.message}';
+
+    } catch (e) {
+
+      return 'Erro inesperado ao tentar recuperar senha: $e';
+
+    }
+
+  }
+
 }
